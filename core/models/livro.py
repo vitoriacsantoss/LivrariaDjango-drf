1105f9fd5a8866394f5343e789b22fe4b0ed743f
vitoriacsantoss <santosviih360@gmail.com>
2024-08-16 14:46:17 -0300

    Criação da entidade para Livro

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Livro(models.Model):
-    nome = models.CharField(max_length=100)
-    site = models.URLField(max_length=200, blank=True, null=True)
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField(default=0,  null=True, blank=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
 
     def __str__(self):
-        return self.nome
+        return f"({self.id}) {self.titulo} ({self.quantidade})"
