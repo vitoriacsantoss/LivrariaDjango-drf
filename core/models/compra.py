ab7ad2ba0a804cce26e922d633d0c55d7d7c1bba
vitoriacsantos <santosviih360@gmail.com>
2024-09-13 15:09:06 -0300

    Inclusão do total da compra na listagem de compras / finalizado

--- core/models/compra.py
+++ core/models/compra.py
@@ -10,20 +10,21 @@ class Compra(models.Model):
         PAGO = 3, "Pago"
         ENTREGUE = 4, "Entregue"
 
+
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
     status = models.IntegerField(choices=StatusCompra.choices,  default=StatusCompra.CARRINHO)
 
+    @property
+    def total(self):
+        total = 0
+        for item in self.itens.all():
+            total += item.livro.preco * item.quantidade
+        return total
+        # return sum(item.livro.preco * item.quantidade for item in self.itens.all())
+    def  __str__(self):
+        return f"{self.usuario} {self.status}"   
+    
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
     quantidade = models.IntegerField(default=1)
-
-
-    ...
-    @property
-    def total(self):
-        # total = 0
-        # for item in self.itens.all():
-        #     total += item.livro.preco * item.quantidade
-        # return total
-        return sum(item.livro.preco * item.quantidade for item in self.itens.all())
--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,13 +1,6 @@
 from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
 from core.models import Compra, ItensCompra
 
-class CompraSerializer(ModelSerializer):
-    class Meta:
-        model = Compra
-        fields = ("id", "usuario", "status", "total", "itens")
-        usuario = CharField(source="usuario.email", read_only=True) # inclua essa linha
-        status = CharField(source="get_status_display", read_only=True) # inclua essa linha
-        itens = ItensCompraSerializer(many=True, read_only=True)
 
 class ItensCompraSerializer(ModelSerializer):
     total = SerializerMethodField()
@@ -21,4 +14,10 @@ class ItensCompraSerializer(ModelSerializer):
         depth = 1
 
 
-
+class CompraSerializer(ModelSerializer):
+    class Meta:
+        model = Compra
+        fields = ("id", "usuario", "status", "total", "itens")
+        usuario = CharField(source="usuario.email", read_only=True) # inclua essa linha
+        status = CharField(source="get_status_display", read_only=True) # inclua essa linha
+        itens = ItensCompraSerializer(many=True, read_only=True)
