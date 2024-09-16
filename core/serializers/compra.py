c6f6cf58a9294e224db1277a688e3e818fa081e5
vitoriacsantoss <santosviih360@gmail.com>
2024-09-16 11:42:25 -0300

    update

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -46,6 +46,14 @@ class CriarEditarCompraSerializer(ModelSerializer):
         fields = ("usuario", "itens")
 
 
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
+
     def update(self, instance, validated_data):
         itens = validated_data.pop("itens")
         if itens:
