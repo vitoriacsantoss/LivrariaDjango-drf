089931f8639a6a413a914199a86963f5a6f86a5a
vitoriacsantoss <santosviih360@gmail.com>
2024-09-16 11:11:43 -0300

    Criação de um endpoint para criar novas compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -14,6 +14,14 @@ class ItensCompraSerializer(ModelSerializer):
         depth = 1
 
 
+
+class CriarEditarItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+
+
+
 class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
@@ -21,3 +29,19 @@ class CompraSerializer(ModelSerializer):
         usuario = CharField(source="usuario.email", read_only=True) # inclua essa linha
         status = CharField(source="get_status_display", read_only=True) # inclua essa linha
         itens = ItensCompraSerializer(many=True, read_only=True)
+
+
+class CriarEditarCompraSerializer(ModelSerializer):
+    itens = CriarEditarItensCompraSerializer(many=True) # Aqui mudou
+
+    class Meta:
+        model = Compra
+        fields = ("usuario", "itens")
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
--- core/views/compra.py
+++ core/views/compra.py
@@ -1,9 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 
-from core.models import Compra
-from core.serializers import CompraSerializer
+from core.serializers import CompraSerializer, CriarEditarCompraSerializer
 
 
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
     serializer_class = CompraSerializer
+
+    def get_serializer_class(self):
+        if self.action in ("create", "update"):
+            return CriarEditarCompraSerializer
+        return CompraSerializer
